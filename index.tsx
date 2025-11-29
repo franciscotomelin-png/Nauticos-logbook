44736a794bf04c02ac58e6de668ce24ba2d9ff65
franciscotomelin-png <francisco.tomelin@gmail.com>
2025-11-29 17:18:58 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
+// index.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
